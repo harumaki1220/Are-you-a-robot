051e38cf3408ce7284c1016f49ae02ab73fc2d3a
miyatarutyyy <study.miyata026@gmail.com>
2025-12-21 01:13:47 +0900

    feat: BeginSlide にプロフィール情報を追加 & ボタンの UI を刷新

--- src/components/BeginSlide.tsx
+++ src/components/BeginSlide.tsx
@@ -6,11 +6,54 @@ type BeginSlideProps = {
 
 export default function BeginSlide({ onNext }: BeginSlideProps) {
   return (
-    <button
-      onClick={onNext}
-      className="px-6 py-3 border rounded-md text-sm text-black hover:bg-gray-100 transition"
+    <div
+      className="
+      bg-blue-500 text-white
+      min-h-screen w-full
+      flex  items-center justify-center
+      "
     >
-      Start reCAPTCHA Test
-    </button>
+      <div className="flex items-center gap-6">
+	{/* Profile */}
+	<div
+	  className="
+	  flex flex-col
+	  text-sm
+	  leading-relaxed
+          "
+	>
+	  <div className="text-lg font-medium">
+	 東洋大学情報連携学部
+	  </div>
+	  <div className="text-lg font-medium">
+	 情報技術メディア研究会
+	  </div>
+	  <div className="text-xl font-semibold">
+	 チーム : WE ARE HUMAN
+	  </div>
+        </div>
+
+	{/* Next Button */}
+	<button
+	  onClick={onNext}
+	  className="
+	  w-12 h-12
+	  flex items-center justify-center
+	  transition
+          "
+	>
+	  <div
+	    className="
+	    w-0 h-0
+	    border-t-16 border-b-16 border-l-30
+	    border-t-transparent
+	    border-b-transparent
+	    border-l-white
+	    ml-1
+            "
+	  />
+	</button>
+      </div>
+    </div>
   );
 }
