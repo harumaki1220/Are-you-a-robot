431d72bea4ca93bebd7ce1d5bf36ada68ad4ba77
Akkiy-ckiliy <s1f102502553@iniad.org>
2025-12-21 13:18:12 +0900

    CSS変更

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-import BeginSlide from "@/components/BeginSlide";
 import RecaptchaBox from "@/components/RecaptchaBox";
 
 export default function Home() {
@@ -11,26 +10,8 @@ export default function Home() {
   
   return (
     <div className="overflow-hidden w-screen h-screen bg-white">
-      <motion.div
-	className="flex w-[200vw] h-full"
-	animate={{
-	  x: phase === "begin" ? "0vw" : "-100vw",
-      	}}
-	transition={{
-	  duration: 0.6,
-	  ease: "easeInOut",
-	}}
-      >
-	{/* phase-1: BeginSlide */}
-        <div className="w-screen h-full flex items-center justify-center">
-	  <BeginSlide onNext={() => setPhase("recaptcha")} />
-	</div>
-
-	{/* phase-2: RecaptchaBox */}
-	<div className="w-screen h-full flex items-center justify-center">
 	  <RecaptchaBox />
-	</div>
-      </motion.div>
+
     </div>
   );
 }
